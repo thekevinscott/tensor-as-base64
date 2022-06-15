2a354ff65956df704e71d868982c9c7d79a744ae
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-06-15 13:29:23 -0400

    Comment out test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,18 @@
-import { tensorAsBuffer } from './index';
+import { tensorAsBase64, tensorAsBuffer } from './index';
 import * as tf from '@tensorflow/tfjs';
 
 describe('tensorAsBuffer', () => {
   it('gets a tensor as a buffer', async () => {
-    const t = tf.tensor([[[1, 2, 3]]]);
+    const t: tf.Tensor3D = tf.tensor([[[1, 2, 3]]]);
     const buffer = await tensorAsBuffer(t);
     expect(Array.from(buffer)).toEqual([1, 2, 3, 255]);
   });
 });
+
+// describe('tensorAsBase64', () => {
+//   it('gets a tensor as a base64 string', async () => {
+//     const t: tf.Tensor3D = tf.tensor([[[1, 2, 3]]]);
+//     const src = await tensorAsBase64(t);
+//     expect(src).toEqual('foo');
+//   });
+// });
